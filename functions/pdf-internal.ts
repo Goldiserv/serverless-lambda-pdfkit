505787bdff3de357fdf05cc8c86ee2cd845116b3
peter-c4f <peter@goldiserv.com>
2020-11-03 22:21:26 +1100

    added invoice

---
+++ functions/pdf-internal.ts
@@ -0,0 +1,255 @@
+import PDFDocument from "pdfkit";
+import middy from "@middy/core";
+import doNotWaitForEmptyEventLoop from "@middy/do-not-wait-for-empty-event-loop";
+const fetch = require('node-fetch').default;
+
+const invoiceDataPlaceholder = {
+  shipping: {
+    name: "John Doe",
+    address: "1234 Main Street",
+    city: "San Francisco",
+    state: "CA",
+    country: "US",
+    postal_code: 94111
+  },
+  items: [
+    {
+      item: "TC 100",
+      description: "Toner Cartridge",
+      quantity: 2,
+      amount: 6000
+    },
+    {
+      item: "USB_EXT",
+      description: "USB Cable Extender",
+      quantity: 1,
+      amount: 2000
+    }
+  ],
+  subtotal: 8000,
+  paid: 0,
+  invoice_nr: 1234
+};
+
+const fetchImage = async (src: string) => {
+  const response = await fetch(src);
+  const image = await response.buffer();
+  return image;
+};
+
+async function generateHeader(doc: PDFKit.PDFDocument) {
+  const logo = await fetchImage("https://i.imgur.com/2ff9bM7.png");
+  //console.log({logo});
+  doc
+    .image(logo, 50, 45, { width: 50 })
+    .fillColor("#444444")
+    .fontSize(20)
+    .text("ACME Inc.", 110, 57)
+    .fontSize(10)
+    .text("ACME Inc.", 200, 50, { align: "right" })
+    .text("123 Main Street", 200, 65, { align: "right" })
+    .text("New York, NY, 10025", 200, 80, { align: "right" })
+    .moveDown();
+}
+
+function generateCustomerInformation(doc: PDFKit.PDFDocument, invoice: any) {
+  doc
+    .fillColor("#444444")
+    .fontSize(20)
+    .text("Invoice", 50, 160);
+
+  generateHr(doc, 185);
+
+  const customerInformationTop = 200;
+
+  doc
+    .fontSize(10)
+    .text("Invoice Number:", 50, customerInformationTop)
+    .font("Helvetica-Bold")
+    .text(invoice.invoice_nr, 150, customerInformationTop)
+    .font("Helvetica")
+    .text("Invoice Date:", 50, customerInformationTop + 15)
+    .text(formatDate(new Date()), 150, customerInformationTop + 15)
+    .text("Balance Due:", 50, customerInformationTop + 30)
+    .text(
+      formatCurrency(invoice.subtotal - invoice.paid),
+      150,
+      customerInformationTop + 30
+    )
+
+    .font("Helvetica-Bold")
+    .text(invoice.shipping.name, 300, customerInformationTop)
+    .font("Helvetica")
+    .text(invoice.shipping.address, 300, customerInformationTop + 15)
+    .text(
+      invoice.shipping.city +
+      ", " +
+      invoice.shipping.state +
+      ", " +
+      invoice.shipping.country,
+      300,
+      customerInformationTop + 30
+    )
+    .moveDown();
+
+  generateHr(doc, 252);
+}
+
+function generateInvoiceTable(doc: PDFKit.PDFDocument, invoice: any) {
+  let i;
+  const invoiceTableTop = 330;
+
+  doc.font("Helvetica-Bold");
+  generateTableRow(
+    doc,
+    invoiceTableTop,
+    "Item",
+    "Description",
+    "Unit Cost",
+    "Quantity",
+    "Line Total"
+  );
+  generateHr(doc, invoiceTableTop + 20);
+  doc.font("Helvetica");
+
+  for (i = 0; i < invoice.items.length; i++) {
+    const item = invoice.items[i];
+    const position = invoiceTableTop + (i + 1) * 30;
+    generateTableRow(
+      doc,
+      position,
+      item.item,
+      item.description,
+      formatCurrency(item.amount / item.quantity),
+      item.quantity,
+      formatCurrency(item.amount)
+    );
+
+    generateHr(doc, position + 20);
+  }
+
+  const subtotalPosition = invoiceTableTop + (i + 1) * 30;
+  generateTableRow(
+    doc,
+    subtotalPosition,
+    "",
+    "",
+    "Subtotal",
+    "",
+    formatCurrency(invoice.subtotal)
+  );
+
+  const paidToDatePosition = subtotalPosition + 20;
+  generateTableRow(
+    doc,
+    paidToDatePosition,
+    "",
+    "",
+    "Paid To Date",
+    "",
+    formatCurrency(invoice.paid)
+  );
+
+  const duePosition = paidToDatePosition + 25;
+  doc.font("Helvetica-Bold");
+  generateTableRow(
+    doc,
+    duePosition,
+    "",
+    "",
+    "Balance Due",
+    "",
+    formatCurrency(invoice.subtotal - invoice.paid)
+  );
+  doc.font("Helvetica");
+}
+
+function generateFooter(doc: PDFKit.PDFDocument) {
+  doc
+    .fontSize(10)
+    .text(
+      "Payment is due in 30 days. Thank you for your business.",
+      50,
+      780,
+      { align: "center", width: 500 }
+    );
+}
+
+function generateTableRow(
+  doc: PDFKit.PDFDocument,
+  y: number,
+  item: string,
+  description: string,
+  unitCost: string,
+  quantity: string,
+  lineTotal: string
+) {
+  doc
+    .fontSize(10)
+    .text(item, 50, y)
+    .text(description, 150, y)
+    .text(unitCost, 280, y, { width: 90, align: "right" })
+    .text(quantity, 370, y, { width: 90, align: "right" })
+    .text(lineTotal, 0, y, { align: "right" });
+}
+
+function generateHr(doc: PDFKit.PDFDocument, y: number) {
+  doc
+    .strokeColor("#aaaaaa")
+    .lineWidth(1)
+    .moveTo(50, y)
+    .lineTo(550, y)
+    .stroke();
+}
+
+function formatCurrency(cents: number) {
+  return "$" + (cents / 100).toFixed(2);
+}
+
+function formatDate(date: any) {
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
+  return year + "/" + month + "/" + day;
+}
+
+const generatePdf = async (invoiceData: any): Promise<Buffer> => {
+  return new Promise(async (resolve: Function, reject: Function): Promise<any> => {
+    let doc = new PDFDocument({ size: "A4", margin: 50 });
+
+    try {
+      await generateHeader(doc);
+      generateCustomerInformation(doc, invoiceData);
+      generateInvoiceTable(doc, invoiceData);
+      generateFooter(doc);      
+    } catch (err) {
+      console.log({ err });
+	}
+
+	doc.end();
+
+    const buffers: any = [];
+    doc.on("data", buffers.push.bind(buffers));
+    doc.on("end", () => {
+      const pdfData = Buffer.concat(buffers);
+      resolve(pdfData);
+    });
+  });
+};
+
+export const handler = async (event: any, context: any) => {
+
+  const stream = await generatePdf(invoiceDataPlaceholder);
+  return {
+    statusCode: 200,
+    isBase64Encoded: true,
+    headers: {
+      "Content-type": "application/pdf"
+    },
+    body: stream.toString("base64")
+  };
+  
+};
+
+export const generate = middy(handler).use(doNotWaitForEmptyEventLoop());
